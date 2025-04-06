c595567acf755638be51ca6c6c72620d2b43eecc
PK109 <kuta.przemyslaw@gmail.com>
2025-04-06 20:34:18 +0000

    Updated orchestration flows

--- kestra/main.tf
+++ kestra/main.tf
@@ -8,35 +8,41 @@ terraform {
 }
 
 provider "kestra" {
-  # mandatory, the URL for kestra
   url = "http://localhost:8080"
-
-  # optional basic auth username
   username = "admin@localhost.dev"
   password = "kestra"
-
+}
+variable "namespace" {
+  default = "dev"
+  description = "Name of workspace to be used. Do not change."
 }
 
 resource "kestra_flow" "gcp_kv_setup" {
-  namespace = "dev"
+  namespace = var.namespace
   flow_id = "gcp_kv_setup"
-  content = file("flows/gcp_kv_setup.yml")
+  content = file("flows/${var.namespace}/gcp_kv_setup.yml")
 }
 
-resource "kestra_flow" "subflow_dev" {
-  namespace = "dev"
-  flow_id = "subflow_dev"
-  content = file("flows/subflow_dev.yml")
+resource "kestra_flow" "extract_raw_data" {
+  namespace = var.namespace
+  flow_id = "extract_raw_data"
+  content = file("flows/${var.namespace}/extract_raw_data.yml")
 }
 
-resource "kestra_flow" "test_subflow" {
-  namespace = "dev"
-  flow_id = "test_subflow"
-  content = file("flows/test_subflow.yml")
+resource "kestra_flow" "batch_extract" {
+  namespace = var.namespace
+  flow_id = "batch_extract"
+  content = file("flows/${var.namespace}/batch_extract.yml")
 }
 
-resource "kestra_flow" "data_daily_ingestion" {
-  namespace = "dev"
-  flow_id = "data_daily_ingestion"
-  content = file("flows/data_daily_ingestion.yml")
+resource "kestra_flow" "ingest_to_bq" {
+  namespace = var.namespace
+  flow_id = "ingest_to_bq"
+  content = file("flows/${var.namespace}/ingest_to_bq.yml")
+}
+
+resource "kestra_flow" "push_to_git" {
+  namespace = "system"
+  flow_id = "push_to_git"
+  content = file("flows/system/push_to_git.yml")
 }
